ffb9aa64c841fc84c29dbdfa954935b84b975450
ffabhishek116@gmail.com <ffabhishek116@gmail.com>
2026-02-25 08:09:25 -0800

    Restaurant Website

---
+++ script.js
@@ -0,0 +1,34 @@
+// console.log("Hello Mitron!");
+// function Car(brand, speed){
+//     this.brand = brand;
+//     this.speed = speed;
+// }
+// Car.prototype.increaseSpeed = function(){
+//     this.speed+= 10;
+//     console.log(this.speed);
+// }
+// let Tata = new Car("Tata",120);
+// let Honda = new Car("Honda",122);
+// Tata.increaseSpeed();
+// Honda.increaseSpeed();
+
+
+// function User(name){
+//     this.name = name;
+// }
+// User.prototype.role = "user";
+// let abhi = new User("Abhishek");
+// abhi.role = "Admin";
+// console.log(abhi.role);
+
+// function Student(name){
+//     this.name = name;
+// }
+// Student.prototype.collage = "ABC University";
+// let s = new Student("Abhishek");
+// console.log(s.hasOwnProperty("name"));
+// console.log(s.hasOwnProperty("collage"));
+
+
+
+
